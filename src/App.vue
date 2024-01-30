7c53e299d313244845adac71d99ad2a9091e91b6
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-01-30 16:21:52 +0200

    instagram feed added

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,10 @@ import EventCalendar from "./components/EventCalendar.vue";
 import Shirts from "./components/Shirts.vue";
 import Groups from "./components/Groups.vue";
 import Content from "./components/Content.vue";
+import InstaFeed from "./components/InstaFeed.vue";
 import About from "./components/About.vue";
 import eventData from "./assets/eventData.json";
+import postLink from "./assets/postLink.json";
 </script>
 
 <template>
@@ -18,6 +20,7 @@ import eventData from "./assets/eventData.json";
   <Shirts></Shirts>
   <Groups></Groups>
   <Content></Content>
+  <InstaFeed v-for="post in postLink" :details="post"></InstaFeed>
   <About></About>
 </template>
 
---
+++ src/components/InstaFeed.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  details: Object,
+});
+</script>
+<template>
+  <div
+    class="flex flex-col xl:flex-row bg-amber-100 px-8 xl:px-28 pb-20 justify-center"
+  >
+    <div class="p-5">
+      <iframe
+        src="{{details.link}}/embed/"
+        width="400"
+        height="480"
+        frameborder="0"
+        scrolling="no"
+        allowtransparency="true"
+      ></iframe>
+    </div>
+  </div>
+
+  <!-- Instagram feed -->
+</template>
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
