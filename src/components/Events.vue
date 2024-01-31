e40849e61b2d735292160089671c2373d80d10d2
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-01-31 16:36:36 +0200

    events mobile design padding and margin changed

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -6,8 +6,8 @@
     <div
       class="flex flex-col xl:flex-row justify-center items-center text-5xl font-bold my-14"
     >
-      <div class="w-96 xl:w-1/2 mb-6">
-        <p class="text-center text-5xl xl:text-5xl mb-6">
+      <div class="w-80 xl:w-1/2 mb-6">
+        <p class="text-center text-4xl xl:text-5xl mb-6">
           Találkozzunk minden szerdán 19 órától a Berthelot 5-nél!
         </p>
         <hr class="my-2 mx-36 xl:my-10 xl:mx-60 border-1 border-black" />
@@ -28,7 +28,7 @@
     <a
       href="events.html"
       target="_blank"
-      class="bg-black rounded-xl py-4 mx-16 mb-6 inline-block"
+      class="bg-black rounded-xl py-4 mx-12 mb-6"
     >
       <p class="text-white font-bold text-2xl text-left ms-6">
         További események
