f6791fef8fdc490eb66593d3e9b547d83e4c93c5
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-01-31 15:20:30 +0200

    instagramm posts size changed for mobile version

--- src/components/InstaFeed.vue
+++ src/components/InstaFeed.vue
@@ -7,8 +7,8 @@ defineProps({
   <div class="p-5 w-full">
     <iframe
       :src="details.link"
-      width="400"
-      height="480"
+      width=" 310 xl:400"
+      height="340 xl:480"
       frameborder="0"
       scrolling="no"
       allowtransparency="true"
