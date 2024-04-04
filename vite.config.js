84c9daebdf15fad01b003d2d648ce025e012d453
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2024-04-04 22:00:27 +0300

    base changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/VaradifiVUE/",
+  base: "/",
   plugins: [vue()],
   server: {
     fs: {
